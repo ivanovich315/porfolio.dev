5bb818049bda4bbe76df8d0561dda2504e6c0cc5
ivanovich315 <ivanegido3@gmail.com>
2024-01-20 17:13:31 +0100

    [MOD] porfolio v2.0 add section components and about me

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,36 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'royal-blue': {
+					'50': '#eff7ff',
+					'100': '#dbecfe',
+					'200': '#bfdefe',
+					'300': '#94cbfc',
+					'400': '#61adf9',
+					'500': '#3c8cf5',
+					'600': '#206ae9',
+					'700': '#1e59d7',
+					'800': '#1f49ae',
+					'900': '#1f4089',
+					'950': '#172854',
+				},
+			},
+			animation: {
+				'background-shine': 'background-shine 2s linear infinite'
+			},
+			keyframes: {
+				'background-shine': {
+					'from': {
+						'backgroundPosition': '0 0'
+					},
+					'to': {
+						'backgroundPosition': '-200% 0'
+					}
+				}
+			}
+		},
 	},
 	plugins: [],
 }
